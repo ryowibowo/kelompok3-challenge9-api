7f9b40d4b6178bcd59a6050f06a5e9957450eef6
Satrio Wibowo <5765615-satriowibowo@users.noreply.gitlab.com>
2021-11-13 22:00:01 +0700

    add express

---
+++ index.js
@@ -0,0 +1,27 @@
+// import express
+const express = require('express');
+const app = express();
+
+// import passport
+const passport = require('./lib/passport');
+
+// get port from env, if env not set, use 3000
+const port = process.env.port || 4000;
+
+// import routes
+const router = require('./routes');
+
+// use middleware for request parsing
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// use passport for jwt auth
+app.use(passport.initialize());
+
+// use route
+app.use(router);
+
+// run app in port specified
+app.listen(port, () => {
+    console.log(`App is running in http://localhost:${port}`);
+});
