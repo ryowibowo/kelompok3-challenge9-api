8d0964ee8f797adf36b84903cd2c50e0b7cfd820
Satrio Wibowo <5765615-satriowibowo@users.noreply.gitlab.com>
2021-11-14 20:02:17 +0700

    update

---
+++ controller/read.js
@@ -0,0 +1,24 @@
+const { user_games,user_game_biodata } = require('../models');
+
+const read = async (req, res) => {
+   try {
+     user_games.findOne({
+        where: { id: req.user.id },
+        include: {
+            model: user_game_biodata,
+            as: 'user_game_biodata'
+        }
+    }).then((read) => {
+        res.json({ read })
+    })
+
+   }
+
+   catch (err) {
+       return res.json(err)
+   }
+}
+
+module.exports = {
+    read
+}
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ const router = express.Router();
 // import router
 const auth = require('./auth');
 const rps = require('./rps')
-
+const update = require('./update')
+const read = require('./read')
 // routes list
 router.use('/auth', auth);
 router.use('/rps',rps)
+router.use('/', update)
+router.use('/', read)
 
 module.exports = router;
