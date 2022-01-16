2c6ae0d8990b943ab4c1be5e91b8310fde02c21c
Satrio Wibowo <5765615-satriowibowo@users.noreply.gitlab.com>
2022-01-16 13:05:08 +0700

    eslint auth

--- controller/auth.js
+++ controller/auth.js
@@ -1,209 +1,185 @@
 // import bcrypt untuk menyimpan & mengecek password dengan cara yg aman
 const bcrypt = require('bcrypt');
-
 // import jwt untuk membuat token
 const jwt = require('jsonwebtoken');
+const lc = require('lower-case');
 
-const { user_games, user_game_biodata } = require('../models');
-
-const lc = require('lower-case')
-
+const { UserGames, UserGameBiodata } = require('../models');
 // method untuk menyimpan password dalam bentuk hash
-const encrypt = (password) => {
-    return bcrypt.hashSync(password, 10);
-}
+// const encrypt = (password) => {
+//     return bcrypt.hashSync(password, 10);
+// }
+const encrypt = (password) => bcrypt.hashSync(password, 10);
 
-const lowerCU = (username) => {
-    return lc.lowerCase(username)
-}
+const lowerCU = (username) => lc.lowerCase(username);
 
-const lowerCE = (email) => {
-    return lc.lowerCase(email)
-}
+const lowerCE = (email) => lc.lowerCase(email);
 
 // method untuk membandingkan password dalam bentuk hash & plain text
-const checkPassword = (password, encryptedPassword,) => {
-    return bcrypt.compareSync(password, encryptedPassword);
-}
+const checkPassword = (password, encryptedPassword) => {
+  bcrypt.compareSync(password, encryptedPassword);
+};
 
 // method untuk membuat token jwt
 const generateToken = (user) => {
-    // Jangan memasukkan password ke dalam payload
-    const payload = {
-        id: user.id,
-        username: user.username,
-    }
+// Jangan memasukkan password ke dalam payload
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
 
-    // Rahasia ini nantinya kita pakai untuk memverifikasi apakah token ini benar-benar berasal dari aplikasi kita
-    const rahasia = process.env.SECRET_KEY;
-
-    // Membuat token dari data-data diatas
-    return jwt.sign(payload, rahasia);
-}
+  const rahasia = process.env.SECRET_KEY;
+  // Membuat token dari data-data diatas
+  return jwt.sign(payload, rahasia);
+};
 
 // method untuk membuat response setelah register
 const format = (user) => {
-    const { id, username, role } = user
-    return {
-        id,
-        username,
-        accessToken: generateToken(user)
-    }
-}
+  const { id, username } = user;
+  return {
+    id,
+    username,
+    accessToken: generateToken(user),
+  };
+};
 
 // controller register
 const register = async (req, res) => {
-    // ambil username, password, dan role dari request body
-    const { username, password, nama, email, umur } = req.body;
-    const lowerCUS = lowerCU(username)
-    const lowerCEM = lowerCE(email)
+  // ambil username, password, dan role dari request body
+  const {
+    username, password, nama, email, umur,
+  } = req.body;
 
-    try {
-        if (req.body.username == "" || req.body.password == "") {
-            return res.status(409).json({
-                "status": "error",
-                "message": "blank username or password"
-            })
-        }
+  const lowerCUS = lowerCU(username);
+  const lowerCEM = lowerCE(email);
+
+  try {
+    if (req.body.username === '' || req.body.password === '') {
+      const result = {
+        status: 'error',
+        message: 'blank username or password',
+      };
+      return res.status(409).json(result);
     }
-    catch (err) {
-        return res.status(409).json({
-            "status": "error",
-            "message": err
-        })
+  } catch (err) {
+    return res.status(409).json(err);
+  }
+  try {
+    // cek apakah user sudah ada
+    const user = await UserGames.findOne({
+      where: { username: lowerCUS },
+    });
+
+    // jika user ditemukan
+    if (user) {
+      const result = {
+        status: 'error',
+        message: 'Username Telah Terdaftar',
+      };
+      return res.status(409).json(result);
     }
-    try {
-        // cek apakah user sudah ada
-        const user = await user_games.findOne({
-            where: { username: lowerCUS }
-        });
+  } catch (err) {
+    return res.status(409).json(err);
+  }
 
-        // jika user ditemukan
-        if (user) {
-            return res.status(409).json({
-                "status": "error",
-                "message": 'Username Telah Terdaftar'
-            });
-        }
+  try {
+    const data = await UserGameBiodata.findOne({
+      where: { email: lowerCEM },
+    });
 
-
-    } catch (err) {
-        return res.status(409).json({
-            "status": "error",
-            "message": err
-        })
+    if (data) {
+      const result = {
+        status: 'error',
+        message: 'Email Telah Terdaftar',
+      };
+      return res.status(409).json(result);
     }
+  } catch (err) {
+    return res.status(409).json(err);
+  }
 
-    try {
-        const data = await user_game_biodata.findOne({
-            where: { email: lowerCEM }
-        })
+  try {
+    // mengubah password menjadi hash
+    const encryptedPassword = encrypt(password);
 
-        if (data) {
-            return res.status(409).json({
-                "status": "error",
-                "message": "email sudah terdaftar"
-            })
-        }
+    // buat user di tabel
+    const user = await UserGames.create({
+      username,
+      password: encryptedPassword,
+      UserGameBiodata: {
+        nama,
+        email,
+        umur,
+        score: 0,
+      },
+    }, {
+      include: {
+        model: UserGameBiodata,
+        as: 'UserGameBiodata',
+      },
+    });
 
-
-    }
-    catch (err) {
-        return res.status(409).json({
-            "status": "error",
-            "message": err
-        })
-    }
-
-
-    try {
-        // mengubah password menjadi hash
-        const encryptedPassword = encrypt(password);
-
-        // buat user di tabel
-        const user = await user_games.create({
-            username,
-            password: encryptedPassword,
-            user_game_biodata: {
-                nama,
-                email,
-                umur,
-                score: 0,
-            },
-        }, {
-            include: {
-                model: user_game_biodata,
-                as: 'user_game_biodata',
-            },
-        });
-
-        const result = {
-            status: "success",
-            message: "Register Berhasil",
-            data: user
-        };
-        return res.json(result);
-
-    } catch (err) {
-        return res.status(409).json({
-            "status": "error",
-            "message": err
-        })
-    }
-}
+    const result = {
+      status: 'success',
+      message: 'Register Berhasil',
+      data: user,
+    };
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(409).json(err);
+  }
+};
 
 // controller login
 const login = async (req, res) => {
-    // ambil username dan password dari request body
-    const { username, password } = req.body;
+  // ambil username dan password dari request body
+  const { username, password } = req.body;
 
-    // siapkan variable penampung user
-    let user = {};
+  // siapkan variable penampung user
+  let user = {};
 
-    try {
-        // cari user berdasar username
-        user = await user_games.findOne({
-            where: { username }
-        })
-    } catch (err) {
-        return res.status(409).json({
-            "status": "error",
-            "message": err
-        })
-    }
+  try {
+    // cari user berdasar username
+    user = await UserGames.findOne({
+      where: { username },
+    });
+  } catch (err) {
+    return res.status(409).json(err);
+  }
 
-    // cek apakah user tidak ditemukan
-    if (!user) {
-        const result = {
-            status: "Failed",
-            message: "User Tidak Ditemukan"
-        };
-        return res.status(409).json(result);
-    }
-
-    // bandingkan password dari request body dengan dari database
-    const isPasswordValid = checkPassword(password, user.password);
-
-    // jika tidak sesuai
-    if (!isPasswordValid) {
-        const result = {
-            status: "Failed",
-            message: "Password Salah"
-        };
-        return res.status(409).json({result});
-    }
-
-    // jika sesuai
+  // cek apakah user tidak ditemukan
+  if (!user) {
     const result = {
-        status: "Success",
-        message: "Login Berhasil",
-        accessToken: generateToken(user),
-        username:username
+      status: 'Failed',
+      message: 'User Tidak Ditemukan',
     };
-    return res.json(result);
-}
+    return res.status(409).json(result);
+  }
+
+  // bandingkan password dari request body dengan dari database
+  const isPasswordValid = checkPassword(password, user.password);
+
+  // jika tidak sesuai
+  if (!isPasswordValid) {
+    const result = {
+      status: 'Failed',
+      message: 'Password Salah',
+    };
+    return res.status(409).json({ result });
+  }
+
+  // jika sesuai
+  const result = {
+    status: 'Success',
+    message: 'Login Berhasil',
+    accessToken: generateToken(user),
+    usernameLogin: username,
+  };
+  return res.json(result);
+};
 
 module.exports = {
-    register,
-    login,
-}
+  format,
+  register,
+  login,
+};
