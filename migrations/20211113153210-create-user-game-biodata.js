3eba473f5b0ab9cf9542994067236f19abc0e061
faluthfi <faikar.al@gmail.com>
2022-01-24 01:38:11 +0700

    fix

--- migrations/20211113153210-create-user-game-biodata.js
+++ migrations/20211113153210-create-user-game-biodata.js
@@ -1,4 +1,3 @@
-'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('UserGameBiodata', {
@@ -43,7 +42,7 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.dropTable('UserGameBiodata');
   },
 };
