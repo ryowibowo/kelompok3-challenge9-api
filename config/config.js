3671079409da4517dd16a3f6499f4358ebad3ecf
Risuii <farisfhm97@gmail.com>
2022-01-25 13:41:06 +0700

    fix config

--- config/config.js
+++ config/config.js
@@ -1,67 +1,68 @@
 require('dotenv').config();
-// module.exports={
-//   "development": {
-//     "username": process.env.DB_USERNAME,
-//     "password": process.env.DB_PASSWORD,
-//     "database": process.env.DB_NAME,
-//     "host": process.env.DB_HOST,
-//     "dialect": "postgres",
-//     "dialectOptions": {
-//       "ssl": {
-//         "require":true,
-//         "rejectUnauthorized":false
-//       }
-//     }
-//   },
-//   "test": {
-//     "username": process.env.DB_USERNAME,
-//     "password": process.env.DB_PASSWORD,
-//     "database": process.env.DB_NAME,
-//     "host": process.env.DB_HOST,
-//     "dialect": "postgres",
-//     "dialectOptions": {
-//       "ssl": {
-//         "require":true,
-//         "rejectUnauthorized":false
-//       }
-//     }
-//   },
-//   "production": {
-//     "username": process.env.DB_USERNAME,
-//     "password": process.env.DB_PASSWORD,
-//     "database": process.env.DB_NAME,
-//     "host": process.env.DB_HOST,
-//     "dialect": "postgres",
-//     "dialectOptions": {
-//       "ssl": {
-//         "require":true,
-//         "rejectUnauthorized":false
-//       }
-//     }
-//   }
-// }
-module.exports = {
+module.exports={
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require":true,
+        "rejectUnauthorized":false
+      }
+    }
+  },
+  "test": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require":true,
+        "rejectUnauthorized":false
+      }
+    }
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require":true,
+        "rejectUnauthorized":false
+      }
+    }
+  }
+}
 
-  development: {
-    username: 'postgres',
-    password: 'babehlo123',
-    database: 'project1',
-    host: 'localhost',
-    dialect: 'postgres',
-  },
-  test: {
-    username: 'postgres',
-    password: 'babehlo123',
-    database: 'project1_test',
-    host: 'localhost',
-    dialect: 'postgres',
-  },
-  production: {
-    username: 'postgres',
-    password: 'babehlo123',
-    database: 'project1_production',
-    host: 'localhost',
-    dialect: 'postgres',
-  },
+// module.exports = {
 
-};
+//   development: {
+//     username: 'postgres',
+//     password: 'babehlo123',
+//     database: 'project1',
+//     host: 'localhost',
+//     dialect: 'postgres',
+//   },
+//   test: {
+//     username: 'postgres',
+//     password: 'babehlo123',
+//     database: 'project1_test',
+//     host: 'localhost',
+//     dialect: 'postgres',
+//   },
+//   production: {
+//     username: 'postgres',
+//     password: 'babehlo123',
+//     database: 'project1_production',
+//     host: 'localhost',
+//     dialect: 'postgres',
+//   },
+
+// };
